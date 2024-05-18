c61f75147387767f91ffae918abaf86a41ee3a93
Giovanna Nogueira <giovannarnogueira@gmail.com>
2024-05-17 23:13:17 -0300

    feat: adicionando page e react router dom

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-function App() {
-    return (
-        <h1 className="text-3xl funderline text-orange-300">Hello world!</h1>
-    );
-}
-
-export default App;
---
+++ src/app/App.tsx
@@ -0,0 +1,7 @@
+import RouterApp from "./routes";
+
+function App() {
+    return <RouterApp />;
+}
+
+export default App;
