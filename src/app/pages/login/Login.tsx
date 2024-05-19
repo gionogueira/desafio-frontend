8c8b011cccdfc65c6690efe923a829800b1f6629
Giovanna Nogueira <giovannarnogueira@gmail.com>
2024-05-18 22:46:03 -0300

    feat: implementando react hooks

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,29 +1,57 @@
-import { useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
+    const inputPasswordRef = useRef<HTMLInputElement>(null);
     const navigate = useNavigate();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    const emailLength = useMemo(() => {
+        console.log("Executou!");
+        return email.length * 1000;
+    }, [email.length]);
+
+    useEffect(() => {
+        // if (window.confirm("Você é um usuário?")) {
+        //     console.log("Sim!");
+        // } else {
+        //     console.log("Não!");
+        // }
+    }, []);
+
+    useEffect(() => {
+        console.log(email);
+    }, [email]);
+
+    useEffect(() => {
+        console.log(password);
+    }, [password]);
 
     const handleClick = () => {
         navigate("/pagina-inicial");
     };
 
-    const handleEntrar = () => {
+    const handleEntrar = useCallback(() => {
         console.log(email);
         console.log(password);
-    };
+    }, [email, password]);
 
     return (
         <div>
             <form>
+                <p>Quantidade de caracteres no email: {emailLength}</p>
                 <label>
                     <span>E-mail</span>
                     <input
                         type="text"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
+                        onKeyDown={(e) =>
+                            e.key === "Enter"
+                                ? inputPasswordRef.current?.focus()
+                                : undefined
+                        }
                     />
                 </label>
 
@@ -33,6 +61,7 @@ function Login() {
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
+                        ref={inputPasswordRef}
                     />
                 </label>
 
